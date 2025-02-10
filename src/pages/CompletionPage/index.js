c3029a456e5c800f7b6e7750dc9755d6e78246e5
Jung-ji-hyeon <ejxldortm@naver.com>
2025-02-10 17:27:40 +0900

    axois 활용

--- src/pages/CompletionPage/index.js
+++ src/pages/CompletionPage/index.js
@@ -1,9 +1,15 @@
+import axios from 'axios';
 import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 
 import testState from '../../stores/test/atom';
 function CompletionPage() {
   const test = useRecoilValue(testState);
+
+  axios.get('http://localhost:3001/surveys').then((res) => {
+    console.log('res', res.data);
+  });
+
   return <CompletionPageWrapper>{test}</CompletionPageWrapper>;
 }
 
